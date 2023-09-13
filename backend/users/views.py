9739ceb01246e82bf69cc97789bba53b366b5fb7
Gleb Kondratev <dr.buldog@yandex.ru>
2023-09-13 21:53:09 +0200

    After review.

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.core.exceptions import ValidationError
 
 from .models import Subscription, User
 from .serializers import SubscriptionSerializer, SubscribeSerializer
