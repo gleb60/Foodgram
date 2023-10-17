623a0c30f2f8d91db2a2dd536cecb099eddc529a
Gleb Kondratev <dr.buldog@yandex.ru>
2023-10-17 11:39:58 +0200

    Little edit.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,7 +72,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
